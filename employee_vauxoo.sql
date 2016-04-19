9b8e094b7e37cad2385f44ef70f8a6505d47944a
apolinar1994 <sebastiaj043@gmail.com>
2016-04-19 04:27:19 -0500

    Update employee_vauxoo.sql

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -15,28 +15,5 @@ INSERT INTO employee (id,first_name,last_name) VALUES (3,'roman','felix');
 INSERT INTO employee (id,first_name,last_name) VALUES (4,'abel','miguel');
 
 
-CREATE TABLE employee_department (
-	id INTEGER PRIMARY KEY, 
-	name VARCHAR(30),
-	description VARCHAR(120)
-	
-);
-
-INSERT INTO employee_departament (id,first_name,last_name) VALUES (1,'sistemas','encargado de admistrar los equipos de computos');
-INSERT INTO employee_departament (id,first_name,last_name) VALUES (2,'logistica','encargado de llevar a cabo la logistica de la empresa');
-INSERT INTO employee_departament (id,first_name,last_name) VALUES (3,'staff','coordina a los integrantes del grupo para realizar diferentes actividades');
-INSERT INTO employee_departament (id,first_name,last_name) VALUES (4,'bioquimica','cordinador del area de quimica y fisica');
-INSERT INTO employee_departament (id,first_name,last_name) VALUES (5,'redes','se encarga de revisar y mantener estable la red de la empresa');
-INSERT INTO employee_departament (id,first_name,last_name) VALUES (6,'electronica','coordinador del area de electronica');
-
-CREATE TABLE employee_hobby (
-id INTEGER PRIMARY KEY,
-name VARCHAR(25),
-description VARCHAR(30)
-
-);
-INSERT INTO employee_hobby(id,name,description) values (1,'jugar','juego de futbol');
-INSERT INTO employee_hobby(id,name,description) values (2,'cantar','cantos romanticos');
-INSERT INTO employee_hobby(id,name,description) values (3,'leer','me gusta leer mucho');
 
 -- ...
